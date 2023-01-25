52a2e0b9820781e569e35aa2d3d21f8cb5b37454
파링 <paring@pikokr.dev>
2023-01-25 21:31:36 +0900

    feat: compare

--- generate-body.sh
+++ generate-body.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo '{}' | jq ".i = \"$MISSKEY_TOKEN\" | .text = \"Pariring 인스턴스 업데이트가 시작되었습니다.\\n버전: \($(cat ../version))\" | .visibility = \"home\"" > ../content.json
+PREV=$(cat ../state/prev)
+CURR=$(cat ../state/current)
+
+echo '{}' | jq ".i = \"$MISSKEY_TOKEN\" | .text = \"Pariring 인스턴스 업데이트가 시작되었습니다.\\n버전: \($(cat ../version))\\n변경사항: [클릭](https://github.com/misskey-dev/misskey/compare/$PREV..$CURR)\" | .visibility = \"home\"" > ../content.json
