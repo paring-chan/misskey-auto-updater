57407ca5f3861f280ace0245ad87c5c61551c012
파링 <paring@pikokr.dev>
2023-01-25 21:27:48 +0900

    fix: version generate

--- generate-body.sh
+++ generate-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo '{}' | jq ".i = \"$MISSKEY_TOKEN\" | .text = \"Pariring 인스턴스 업데이트가 시작 되었습니다.\\n버전: \($(cat ../version))\" | .visibility = \"home\"" > ../content.json
+echo '{}' | jq ".i = \"$MISSKEY_TOKEN\" | .text = \"Pariring 인스턴스 업데이트가 시작되었습니다.\\n버전: \($(cat ../version))\" | .visibility = \"home\"" > ../content.json
