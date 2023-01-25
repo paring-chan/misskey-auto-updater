ac219197575707c4b965963377d0c0f2141d5a43
파링 <paring@pikokr.dev>
2023-01-25 21:33:41 +0900

    fix: syntax error

--- generate-body.sh
+++ generate-body.sh
@@ -3,4 +3,4 @@
 PREV=$(cat ../state/prev)
 CURR=$(cat ../state/current)
 
-echo '{}' | jq ".i = \"$MISSKEY_TOKEN\" | .text = \"Pariring 인스턴스 업데이트가 시작되었습니다.\\n버전: \($(cat ../version))\\n변경사항: [클릭](https://github.com/misskey-dev/misskey/compare/$PREV..$CURR)\" | .visibility = \"home\"" > ../content.json
+echo '{}' | jq ".i = \"$MISSKEY_TOKEN\" | .text = \"Pariring 인스턴스 업데이트가 시작되었습니다.\\n버전: ($(cat ../version))\\n변경사항: [클릭](https://github.com/misskey-dev/misskey/compare/$PREV..$CURR)\" | .visibility = \"home\"" > ../content.json
