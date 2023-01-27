61e97f42cd82cd279268c57c14594ec386ac21a1
파링 <paring@pikokr.dev>
2023-01-27 12:56:46 +0900

    Update set-version.sh

--- set-version.sh
+++ set-version.sh
@@ -10,4 +10,6 @@ VERSION="$(cat ../misskey/package.json | jq .version -r)-$(echo -n $SHA)"
 
 cat ../misskey/package.json | jq ".version=\"$VERSION\"" | tee ../misskey/package.json
 
+sed -i -e 's/ && compareVersions.*1//g' ../misskey/packages/frontend/src/init.ts
+
 echo -n $VERSION > ../version
